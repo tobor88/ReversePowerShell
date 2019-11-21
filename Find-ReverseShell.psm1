b955f9bb8f0dc2f243f478f9ff5a768a3f9a0509
tobor <rosborne@osbornepro.com>
2019-11-21 16:30:55 -0700

    Add files via upload
    In progress still.
    
    This is a command that is to be run in Task Scheduler to discover when the reverse shell was executed. I have not found a way to identify powercat yet. The command is to obvious and may be changed. It seems like that may be the only way to identify it

---
+++ Find-ReverseShell.psm1
@@ -0,0 +1,44 @@
+﻿Function Find-ReverseShell {
+    [CmdletBinding()]
+        param(
+            [Parameter(
+                Mandatory=$False,
+                Positiion=0,
+                HelpMessage="Enter a hostname, FQDN, or an IPv4 address")]
+            [string]$ComputerName
+            ) # End param
+
+BEGIN
+{
+
+    If (!($ComputerName.IsPresent))
+    {
+
+        $ComputerName = $env:COMPUTERNAME
+        
+    } # End If
+
+} # End BEGIN
+
+PROCESS
+{
+
+    Write-Host "Checking for Reverse Shells that connect to a System.Net.Sockets.TcpListener object" -ForegroundColor Cyan
+
+    $TcpListenerCheck = Get-WinEvent -LogName Security -FilterXPath "*[System[EventID=4656 and TimeCreated[timediff(@SystemTime) <= 86400000]] and EventData[Data[@Name='SubjectUserName']!='paessler'] and EventData[Data[@Name='ObjectServer']='WS-Management Listener']]" 
+
+
+    Write-Host "Checking for a Reverse Shell created by a tool such as PowerCat that execute Reverse Shell commands as a process using WSMAN"
+
+    $PowerCatListenerCheck = Get-WinEvent -LogName Security -FilterXPath "*[System[EventID=4656 and TimeCreated[timediff(@SystemTime) <= 86400000]] and EventData[Data[@Name='ObjectName']='\REGISTRY\MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\WSMAN'] and EventData[Data[@Name='SubjectUserName']!=`'$ComputerName$`']]" | Select Message | fl *
+
+} # End PROCESS
+
+END
+{
+
+    Write-Host "Reverse Shell check has completed. The last 24 hours have been verified to not have any shells."
+
+} # End END
+
+} # End Function Find-ReverseShell
